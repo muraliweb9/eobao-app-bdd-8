1714442a270713ce30a181568a4cad89ee0e404a
muraliweb9 <muraliweb@hotmail.com>
2023-09-30 14:03:50 +0100

    Initial

--- src/test/java/com/murali/natwest/eobao8/Eobao8IntegrationTest.java
+++ src/test/java/com/murali/natwest/eobao8/Eobao8IntegrationTest.java
@@ -2,13 +2,12 @@ package com.murali.natwest.eobao8;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features/",
         glue = "com.murali.natwest.eobao8.steps")
-class Eobao8AppTest {
+public class Eobao8IntegrationTest {
 
 
 }
